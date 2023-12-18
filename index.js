9637a14ff07b80818b9fb1d76f877dbbc4b8be5c
saurabh <saurabh.k@ehostinguk.com>
2023-12-18 19:39:18 +0530

    package

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 const express = require('express');
 const multer = require('multer');
 const Jimp = require('jimp');
-const webpConverter = require('webp-converter');
-require('dotenv').config();
-const sharp = require('sharp');
+require('dotenv').config();;
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -23,22 +21,13 @@ app.post('/upload', upload.fields([{ name: 'mainImage', maxCount: 1 }, { name: '
   
         return res.status(400).json({ error: `Missing or unexpected fields: ${missingFields.join(', ')}` });
       }
+
         const mainImage = files['mainImage'][0];
         const markImage = files['markImage'][0];
 
-        let mainImageBuffer = mainImage.buffer;
-        let resizedBuffer;
-
-        if (mainImage.mimetype === 'image/webp') {
-          mainImageBuffer = await sharp(mainImageBuffer)
-            .toFormat('png')
-            .toBuffer();
-            resizedBuffer = mainImageBuffer;
-        }else{
-          await mainImageBuffer.resize(450, Jimp.AUTO);
-          resizedBuffer = await mainImageBuffer.getBufferAsync(Jimp.AUTO);
-        }
-
+        const mainImageBuffer = await Jimp.read(mainImage.buffer);
+        await mainImageBuffer.resize(450, Jimp.AUTO);
+        const resizedBuffer = await mainImageBuffer.getBufferAsync(Jimp.AUTO);
 
         const watermarkedBuffer = await addWatermark(resizedBuffer, markImage.buffer);
         const watermarkedBase64 = `data:${mainImage.mimetype};base64,${watermarkedBuffer.toString('base64')}`;
